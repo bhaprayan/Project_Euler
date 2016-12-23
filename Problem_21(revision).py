ade669cca9e9fb0667f0c676db62beb5abd67975
Shubhankar Deshpande <shubhankardeshpande@hotmail.com>
2016-12-23 23:33:02 +0000

    Create revision to original solution of problem 21

---
+++ Problem_21(revision).py
@@ -0,0 +1,23 @@
+def Divisor_List(n,m):
+    divisor_list_0 = [] 
+    divisor_list_1 = []   
+    for i in range(n):
+        if n % i == 0:
+            divisor_list_0.append(i)
+    for i in range(m):
+        if m % i == 0:
+            divisor_list_1.append(i)
+    if sum(divisor_list_0) == m and sum(divisor_list_1) == n:
+        return True
+    else:
+        return False
+
+Amicable = []
+
+for i in range(10000):
+    for j in range(10000):
+        if Divisor_List(i,j) == True:
+            Amicable.append(i)
+            Amicable.append(j)
+
+print sum(Amicable)
